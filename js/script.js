649c35cf67c3c8602afbf913159234c1f6b2155a
unknown <kiruba.nandhu@gmail.com>
2021-06-21 09:13:49 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
     $(window).on('load scroll', function(){
         $('.menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+
+        $('section').each(function(){
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top - 100;
+            if(top > offset && top<offset+height)
+            {
+                
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
     });
     
 });
